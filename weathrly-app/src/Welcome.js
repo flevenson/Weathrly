96342c897e4dde016a9ac538ad4595180a127c8c
Alexander Ela <ela.alexander@gmail.com>
2018-09-04 19:37:11 -0600

    Add Welcome and Banner components

---
+++ weathrly-app/src/Welcome.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Welcome.css';
+
+class Welcome extends Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return(
+			<div className="welcome">
+
+			</div>
+			);
+	}
+}
+
+export default Welcome;
